eb76c55fcc72295f715d5e0e3b48cd05fab36054
sarahGamal92 <al361242@uji.es>
2021-08-20 17:35:32 +0200

    Update bikeshare_Sarah.py

--- bikeshare_Sarah.py
+++ bikeshare_Sarah.py
@@ -235,9 +235,9 @@ def main():
         city, month, day = get_filters(city, month, day)
         df = load_data(city, month, day)
 
-        #time_stats(df)
-        #station_stats(df)
-        #trip_duration_stats(df)
+        time_stats(df)
+        station_stats(df)
+        trip_duration_stats(df)
         user_stats(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
